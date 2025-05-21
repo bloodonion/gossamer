49eba7d57ed9676e4993c6483251e00355f682ed
user <user@localhost.localdomain>
2025-05-22 01:52:23 +1000

    packages

--- playbooks/kvm_build_notes.sh
+++ playbooks/kvm_build_notes.sh
@@ -22,12 +22,19 @@ lsmod | grep kvm
 sudo nmcli connection show
 sudo nmcli connection delete <interface_name>
 
+# delete physical
+# make bridge
+# add physical to bridge
+# assign dhcp or static to bridge
+
 #create new bridge
 sudo nmcli connection add type bridge con-name br0 ifname br0
+
+sudo nmcli connection add type bridge-slave ifname eno1 master bridge0
 # add a physical interface to the bridge
-sudo nmcli connection add type bridge-slave ifname <interfaceName> master br0
+nmcli c add type bridge-slave ifname eno1 master bridge0
 # modify connection to use dhcp for ip address -- stay static ?
-sudo nmcli connection modify br0 ipv4.method auto
+sudo nmcli connection modify bridge0 ipv4.method auto
 # or... ooorr
 sudo nmcli connection modify br0 ipv4.addresses [IP_address/subnet]
 sudo nmcli connection modify br0 ipv4.gateway [gateway]
@@ -36,17 +43,18 @@ sudo nmcli connection modify br0 ipv4.method manual
 
 
 # bring bridge up 
-sudo nmcli connection up br0
+sudo nmcli connection up bridge0
 # verify
-ip addr show br0
+ip addr show bridge0
 
+# download dependancies - sudo dnf download --downloaddir=. --resolve virt-install
 # CREATE VM
 sudo yum -y install virt-install
 #Adjust user permissions to allow hypervisor to use the iso
 sudo setfacl -m u:qemu:x [file_location]
 
-sudo virt-install --name=ubuntu --ram=3072 --vcpus=2 --file=/var/lib/libvirt/images/ubuntu.img,size=20 --cdrom=Downloads/ubuntu-24.04.1-desktop-amd64.iso --network bridge=br0
-
+sudo virt-install --name=rocky9virt --ram=3072 --vcpus=2 --file=/var/lib/libvirt/images/rocky9virt.img,size=20 --cdrom=--cdrom=/tmp/Rocky-9.5-x86_64-minimal.iso  --network bridge=bridge0
+sudo virt-install --name=rocky9virt --ram=3072 --vcpus=2 --disk path=/var/lib/libvirt/images/rocky9virt.img,size=20 --cdrom=/var/lib/libvirt/images/Rocky-9.5-x86_64-minimal.iso --network bridge=bridge0 --boot cdrom,hd --extra-args="ks=file:/ks.cfg"
 
 # --name=	Custom name of the VM
 # --ram=	Allocated RAM
@@ -55,3 +63,30 @@ sudo virt-install --name=ubuntu --ram=3072 --vcpus=2 --file=/var/lib/libvirt/ima
 # --size=	Allocated file size of the VM
 # --cdrom=	Installation media
 # --network bridge=	Network used for the VM
+
+sudo cat /var/lib/libvirt/dnsmasq/bridge0.leases
+
+arp -an
+
+sudo nmap -sn 192.168.1.0/24
+
+# need a kickstart file or extra options
+sudo virsh console rocky9virt
+
+
+#SE for kickstart 
+sudo chcon -t virt_content_t /var/lib/libvirt/images/ks.cfg
+sudo mv /tmp/Rocky-9.5-x86_64-minimal.iso /var/lib/libvirt/images/
+sudo chcon -t virt_content_t /var/lib/libvirt/images/Rocky-9.5-x86_64-minimal.iso
+
+#advanced kickstart file
+part /boot --fstype=ext4 --size=1024
+part pv.01 --size=18000 --grow
+volgroup vg_root pv.01
+logvol / --fstype=ext4 --size=10000 --name=root --vgname=vg_root
+logvol swap --fstype=swap --size=2048 --name=swap --vgname=vg_root
+
+
+sudo cat /var/log/libvirt/qemu/rocky9virt.log
+
+sudo virsh net-list --all
